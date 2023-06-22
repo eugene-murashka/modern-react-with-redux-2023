572c9d92235a4d52f0d9ce25eeb8201054031c65
Yauheni Murashka <eugene.murashka@gmail.com>
2023-06-22 19:49:48 +0200

    Add css styles for app and animalShow components

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -26,9 +26,10 @@ function AnimalShow({ type }) {
   const heartWidth = 10 + 10 * cliks;
 
   return (
-    <div onClick={handleClick}>
-      <img alt="animal" src={svgMap[type]} />
-      <img 
+    <div className="animal-show" onClick={handleClick}>
+      <img className="animal" alt="animal" src={svgMap[type]} />
+      <img
+        className="heart"
         alt="heart"
         src={heart}
         style={{width: heartWidth}}
