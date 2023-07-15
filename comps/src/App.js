9613da5a1cab2a2c3634a309cca7b2547e1ca54b
Yauheni Murashka <eugene.murashka@gmail.com>
2023-07-15 02:40:00 +0200

    Add buttoms

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,7 +1,23 @@
+import Button from "./Button";
+
 function App() {
     return (
         <div>
-            App
+            <div>
+                <Button success>Button_1</Button>
+            </div>
+            <div>
+                <Button primary>Button_2</Button>
+            </div>
+            <div>
+                <Button secondary>Button_3</Button>
+            </div>
+            <div>
+                <Button danger>Button_4</Button>
+            </div>
+            <div>
+                <Button warning>Button_5</Button>
+            </div>
         </div>
     );
 }
