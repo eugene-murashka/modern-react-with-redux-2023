1eaa2610354fcfe74133da0229082084cc047ece
Yauheni Murashka <eugene.murashka@gmail.com>
2023-07-21 02:06:42 +0200

    Add rest to button for special styles

--- comps/src/Button.js
+++ comps/src/Button.js
@@ -11,21 +11,24 @@ function Button({
     rounded,
     ...rest
 }) {
-    const classes = classNames("flex items-center px-3 py-1.5 border", {
-        "border-blue-500 bg-blue-500": primary,
-        "border-gray-900 bg-gray-900": secondary,
-        "border-green-500 bg-green-500": success,
-        "border-yellow-400 bg-yellow-400": warning,
-        "border-red-500 bg-red-500": danger,
-        "text-white": !outline && (primary || secondary || success || warning || danger),
-        "rounded-full": rounded,
-        "bg-white": outline,
-        "text-blue-500": outline && primary,
-        "text-gray-900": outline && secondary,
-        "text-green-500": outline && success,
-        "text-yellow-400": outline && warning,
-        "text-red-500": outline && danger,
-      });
+    const classes = classNames(
+        rest.className,
+        "flex items-center px-3 py-1.5 border",
+        {
+            "border-blue-500 bg-blue-500": primary,
+            "border-gray-900 bg-gray-900": secondary,
+            "border-green-500 bg-green-500": success,
+            "border-yellow-400 bg-yellow-400": warning,
+            "border-red-500 bg-red-500": danger,
+            "text-white": !outline && (primary || secondary || success || warning || danger),
+            "rounded-full": rounded,
+            "bg-white": outline,
+            "text-blue-500": outline && primary,
+            "text-gray-900": outline && secondary,
+            "text-green-500": outline && success,
+            "text-yellow-400": outline && warning,
+            "text-red-500": outline && danger,
+        });
 
     return (
         <button {...rest} className={classes}>{children}</button>
